94a403761825dab95c6812c776ff712443c8a9bd
xiaona <xiaona_chqdt@163.com>
2014-03-14 15:40:01 +0800

    Initial commit

--- app/store/FunctionStore.js
+++ app/store/FunctionStore.js
@@ -11,9 +11,10 @@ Ext.define('WebInspect.store.FunctionStore', {
     		{id: '05', name: '潮位信息', image: 'resources/images/function/tide.png'},
     		{id: '06', name: '水情信息', image: 'resources/images/function/water.png'},
             {id: '07', name: '雨情信息', image: 'resources/images/function/rain.png'},
-            {id: '08', name: '流量信息', image: 'resources/images/function/rain.png'},
-    		{id: '09', name: '工情信息', image: 'resources/images/function/project.png'},
-            {id: '10', name: '设置', image: 'resources/images/function/setting.png'}
+            {id: '08', name: '流量信息', image: 'resources/images/function/flow.png'},
+            {id: '09', name: '海塘巡查', image: 'resources/images/function/inspect.png'},
+    		{id: '10', name: '工情信息', image: 'resources/images/function/project.png'},
+            {id: '11', name: '设置', image: 'resources/images/function/setting.png'}
         ],
                 
         autoLoad: true
